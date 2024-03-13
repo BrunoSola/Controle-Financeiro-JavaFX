c07fcafa796655940376404f174d26c748dc5f5a
BrunoSola <brunosola3@gmail.com>
2024-03-13 04:28:29 -0300

    Basic MainView Created, files names adjusted

---
+++ src/main/java/sola/bruno/projetocontrolefinanceiro/Main.java
@@ -0,0 +1,31 @@
+package sola.bruno.projetocontrolefinanceiro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/MainView.fxml"));
+            ScrollPane scrollPane = fxmlLoader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
+            stage.setTitle("Controle Financeiro");
+            stage.setScene(mainScene);
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
